0f13365718d4fb6a0179a06af17a47a2ad5f2d5e
Alan Licerio <alicerio2@miners.utep.edu>
2020-12-02 18:05:54 -0700

    Maybe this time works. Deleted main file.

--- lab/methods.py
+++ lab/methods.py
@@ -5,9 +5,11 @@
 # Assignment: Lab 3 - Threaded Video Player
 
 import cv2
+import threading
 from FrameQueue import FrameQueue
 
 VIDEO = "..\clip.mp4" 
+DELIMITER = "\0"
 DELAY = 42
 
 def extractFiles(filename, outQueue) :
@@ -23,7 +25,7 @@ def extractFiles(filename, outQueue) :
         success,image = video.read() 
         i+=1 # increment temp variable
     
-    outQueue.enqueue(None)
+    outQueue.enqueue(DELIMITER)
     print('Process completed')
 
 
@@ -32,13 +34,13 @@ def convertToGray(inQueue, outQueue) :
     i = 0
     input = inQueue.dequeue()
 
-    while input is not None:
+    while input is not DELIMITER:
         frame = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY) # Conversion to gray
         outQueue.enqueue(frame) # Enqueue frame
         i+=1
         input = inQueue.dequeue() # Dequeue next frame
 
-    outQueue.enqueue(None)
+    outQueue.enqueue(DELIMITER)
     print('Process completed')
 
 
@@ -46,7 +48,7 @@ def display(frames) :
     print('Displaying frames...')
     i = 0
     frame = frames.dequeue()
-    while frame is not None:
+    while frame is not DELIMITER:
         cv2.imshow('Video Play', frame)
         if 0xFF == ord("q") and cv2.waitKey(DELAY): # Delay of 42 ms to check if user wants to quit.
             break # Exit loop if condition holds
@@ -55,3 +57,17 @@ def display(frames) :
     
     cv2.destroyAllWindows() # Cleaning opened windows
     print('Process completed')
+
+if __name__ == "__main__":
+    colorFrame = FrameQueue()
+    grayFrame = FrameQueue()
+
+    # Threads
+    extractThread = threading.Thread(target=extractFiles, args=(VIDEO, colorFrame))
+    convertThread = threading.Thread(target=convertToGray, args=(colorFrame, grayFrame))
+    displayThread = threading.Thread(target=display, args=(grayFrame,)) #
+
+    # Start Threads
+    extractThread.start()
+    convertThread.start()
+    displayThread.start()
